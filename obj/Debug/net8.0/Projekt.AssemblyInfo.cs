9a6eefe7508cd125883069020994adc875e2cc84
Michał Sołowiej <89214@student.pb.edu.pl>
2025-01-17 09:47:32 +0100

    mini fix

--- obj/Debug/net8.0/Projekt.AssemblyInfo.cs
+++ obj/Debug/net8.0/Projekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbb249e0e3de5baa4a6d5512dd83439d502a3a13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ff4f214dc4a1b15ebfd3d2e9fcebdb896be69f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
