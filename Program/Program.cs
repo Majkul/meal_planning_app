2fc5fa10e8bcb1bbb2c37708ad3c92c2970d4ff8
Korellek <korelleks@gmail.com>
2025-01-29 21:55:22 +0100

    Fixed/Repurposed UIHandler

--- Program/Program.cs
+++ Program/Program.cs
@@ -42,55 +42,9 @@ public class Program {
         string historyFilePath = "mealHistory.json";
         Meal.MealHistory mealHistory = new Meal.MealHistory();
         mealHistory.LoadFromFile(historyFilePath);
-        int TotalCalories = 0;
-        double TotalProtein = 0;
-        double TotalFat = 0;
-        double TotalCarbohydrates = 0;
-        List<string?> melas_names_in_date = MealServices.GetMealsNamesForDate(mealHistory, today);
-        List<Meal.Meal.MealMemento> melas_in_date = MealServices.GetMealsForDate(mealHistory, today);
-        for(int i=0; i<5; i++)
-        {
-            int TotalRecipeCalories = 0;
-            double TotalRecipeProtein = 0;
-            double TotalRecipeFat = 0;
-            double TotalRecipeCarbohydrates = 0;
-            Console.Write("|");
-            Console.Write(meals[i].PadLeft((111 - meals[i].Length) / 2 + meals[i].Length).PadRight(111));
-            Console.WriteLine("|");
+        
+        UIHandler.CalculateAndDisplaySummary(mealHistory, today, meals);
 
-            Meal.Meal.MealType mealType = (Meal.Meal.MealType)Array.IndexOf(meals, meals[i]);
-            string mealName = melas_names_in_date[i] ?? "No meal";
-            if(mealName!="No meal"){
-                foreach (var recipe in melas_in_date[i].Recipes) {
-                    TotalCalories += recipe.Calories;
-                    TotalRecipeCalories += recipe.Calories;
-
-                    TotalRecipeProtein += recipe.Protein;
-                    TotalProtein += recipe.Protein;
-                    TotalRecipeFat += recipe.Fat;
-                    TotalFat += recipe.Fat;
-                    TotalRecipeCarbohydrates += recipe.Carbohydrates;
-                    TotalCarbohydrates += recipe.Carbohydrates;
-                
-                }
-            }
-
-            Console.Write("|");
-            Console.Write(mealName.PadLeft((111 - mealName.Length) / 2 + mealName.Length).PadRight(111));
-            Console.WriteLine("|");
-            Console.Write("|");
-            string summary = $"Kcal: {TotalRecipeCalories}, Prot.: {TotalRecipeProtein}g, Fats: {TotalRecipeFat}g, Carbs: {TotalRecipeCarbohydrates}g";
-            Console.Write(summary.PadLeft((111 - summary.Length) / 2 + summary.Length).PadRight(111));
-            Console.WriteLine("|");
-            Console.WriteLine(new string('-', 113));
-        }
-
-        //Kalorie z całego dnia
-        Console.Write("|");
-        string summaryAllDay = $"Total:   Kcal: {TotalCalories}, Prot.: {TotalProtein}g, Fats: {TotalFat}g, Carbs: {TotalCarbohydrates}g";
-        Console.Write(summaryAllDay.PadLeft((111 - summaryAllDay.Length) / 2 + summaryAllDay.Length).PadRight(111));
-        Console.WriteLine("|");
-        Console.WriteLine(new string('-', 113));
         //Bazy danychy
         var RecipesDatabase = DB.ConnectionManager.getInstance().getConnection<Recipe.Recipe>("Recipes");
         var ProductsDatabase = DB.ConnectionManager.getInstance().getConnection<Product.Product>("Products");
@@ -153,6 +107,5 @@ public class Program {
             }
         }
     }
-
-} 
+}
 }
